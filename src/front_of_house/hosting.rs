4385c1e6e50f0ae4ffe85c726c687b7ad4ce3287
= <=>
2022-11-02 00:19:59 -0500

    add front of house

---
+++ src/front_of_house/hosting.rs
@@ -0,0 +1,3 @@
+pub fn add_to_waitlist() {
+    println!("Hello I live inside my own file@hosting.rs")
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,49 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod front_of_house;
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn deliver_order() {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+mod back_of_house {
+    pub struct BreakFast {
+        pub toast: String,
+        seasonal_fruit: String,
     }
+
+    impl BreakFast {
+        pub fn summer(toast: &str) -> BreakFast {
+            BreakFast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat() {
+    let mut break_fast = crate::back_of_house::BreakFast::summer("Rye");
+    break_fast.toast = String::from("Mutated");
+}
+
+struct User {
+    userName: String,
+    id: i32,
+}
+
+fn defineUser(mut user: User) -> i32 {
+    println!("{}", user.userName);
+    user.userName = String::from("Aoufi");
+    user.id
+}
+
+fn main() {
+    let mut user: User = User {
+        userName: String::from("Aoufi Abderahmane"),
+        id: 12,
+    };
+    defineUser(user); // ! ownership
+    eat_at_restaurant();
 }
